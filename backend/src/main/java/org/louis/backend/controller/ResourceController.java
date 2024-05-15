d2141610fd1434c5757e415d6e20bfc96e74dd2c
louisBerlin <louis.froch@gmail.com>
2024-05-15 17:10:50 +0200

    test deployment render

--- backend/src/main/java/org/louis/backend/controller/ResourceController.java
+++ backend/src/main/java/org/louis/backend/controller/ResourceController.java
@@ -1,7 +1,10 @@
 package org.louis.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class ResourceController {
@@ -21,4 +24,11 @@ public class ResourceController {
 
 
      */
+
+
+    @RequestMapping(value = {"/*", "/**/{path:[^\\.]*}"}, method = RequestMethod.GET)
+    public ModelAndView redirectToMainPage() {
+        return new ModelAndView("forward:/index.html");
+    }
+
 }
